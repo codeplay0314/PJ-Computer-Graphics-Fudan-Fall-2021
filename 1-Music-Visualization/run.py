c303778b9a22101166eb87e3d49a1ebcb27bbd6c
Erzheng Fu <codeplay0314@gmail.com>
2021-12-27 22:54:49 +0800

    Project 1 completed

--- 1-Music-Visualization/run.py
+++ 1-Music-Visualization/run.py
@@ -122,7 +122,7 @@ steps[0][1] += y
 for i in range(1, len(steps)):
     steps[i][0] += steps[i - 1][0]
     steps[i][1] += steps[i - 1][1]
-# random.shuffle(steps)
+random.shuffle(steps)
 
 for g in tmp_bars:
     gr = []
